1fb308353ba5c2496413ee4e26b63b3b023b98fe
FelipeSierraBorunda <felipesierraborunda@gmail.com>
2025-03-19 23:39:07 -0600

    Asignar frecuencia con regisstros

--- main.c
+++ main.c
@@ -9,34 +9,37 @@ int nota=0;
 int CantidadDeNotas=11;
 int octava=0;
 int YaCambioNota=0;
-uint32_t frecuencia;
+int frecuencia;
 int NotaSonando=0;
 uint32_t duracion;
 uint32_t TiempoDeSilencio=350000; //350 ms
 int NumeroNota=0;
 int CantidadNotasMelodia =0;
+int silencio;
+
+uint32_t BitMask, ValorDesplazadoDivisor;
 // Matriz de duraciones
 uint32_t Duraciones[] = 
 //	  .125 s	  	.25 s		  .5 s		   1 s
 	{125000, 250000, 500000, 1000000};
 // CORCHEA   NEGRA    BLANCA  REDONDA
-// N
-// Matriz de segmentos para los dígitos 0-9
+
+// Matriz de segmentos para los dígitos 0-9     
 uint32_t NotasyOctavas[][8] = 
 {
-    	// 1°oct,  2°oct,   3°oct,     4°oct,    5°oct,    6°oct,    7°oct,      8°oct
-    {32, 64, 128, 256, 512, 1024, 2048, 4096}, // Do
-    {34, 68, 136, 272, 544, 1088, 2176, 4352}, // Do#
-    {36, 72, 144, 288, 576, 1152, 2304, 4608}, // Re
-    {37, 74, 148, 296, 592, 1184, 2368, 4736}, // Re#
-    {38, 76, 152, 304, 608, 1216, 2432, 4864}, // Mi
-    {39, 78, 156, 312, 624, 1248, 2496, 4992}, // Fa
-    {41, 82, 164, 328, 656, 1312, 2624, 5248}, // Fa#
-    {43, 86, 172, 344, 688, 1376, 2752, 5504}, // Sol
-    {44, 88, 176, 352, 704, 1408, 2816, 5632}, // Sol#
-    {46, 92, 184, 368, 736, 1472, 2944, 5888}, // La
-   {47, 94, 188, 376, 752, 1504, 3008, 6016}, // La#
-   {49, 98, 196, 392, 784, 1568, 3136, 6272}, // Si 
+    // 1°oct    2°oct    3°oct    4°oct    5°oct    6°oct    7°oct    8°oct
+    {3270, 6541, 13081, 26163, 52325, 104650, 209300, 418601}, // Do
+    {3465, 6930, 13859, 27718, 55437, 110873, 221746, 443492}, // Do#
+    {3671, 7342, 14683, 29366, 58733, 117466, 234932, 469864}, // Re
+    {3889, 7778, 15556, 31113, 62225, 124451, 248902, 497803}, // Re#
+    {4120, 8241, 16481, 32963, 65925, 131851, 263702, 527404}, // Mi
+    {4365, 8731, 17461, 34923, 69846, 139691, 279383, 558765}, // Fa
+    {4625, 9250, 18500, 36999, 73999, 147998, 295996, 591991}, // Fa#
+    {4900, 9800, 19600, 39200, 78399, 156798, 313596, 627193}, // Sol
+    {5191, 10383, 20765, 41530, 83061, 166122, 332244, 664488}, // Sol#
+    {5500, 11000, 22000, 44000, 88000, 176000, 352000, 704000}, // La (Referencia 440Hz)
+    {5827, 11654, 23308, 46616, 93233, 186466, 372931, 745862}, // La#
+    {6174, 12347, 24694, 49388, 98777, 197553, 395107, 790213}, // Si 
 };
 const char* NombresNotas[] = 
 {
@@ -48,39 +51,57 @@ typedef enum {
     DO = 0, DO_S, RE, RE_S, MI, FA, FA_S, SOL, SOL_S, LA, LA_S, SI
 } Nota;
 
-uint32_t Melodia[][3] = 
+uint32_t Melodia[][4] = 
 {
-    {DO,   4, 1},
-    {RE,   4, 1},
-    {MI,  4, 1},
-    {DO,   4, 1},
-    
-    {DO,   4, 1},
-    {RE,   4, 1},    
-    {MI,   4, 1},  
-    {DO,   4, 1},
-    
-    {MI,   4, 1},  
-    {FA,   4, 1},
-    {SOL,   4, 2},
-    
-     {MI,   4, 1},  
-    {FA,   4, 1},
-    {SOL,   4, 2},
-    
-    {SOL,   4, 0},
-    {LA,   4, 0},    
-    {SOL,   4, 0},  
-    			{FA,   		4, 		0},
-    {MI,   4, 1},
-    {DO,   4, 1},
-};
+ {DO, 3, 1, 0}, {DO, 3, 1, 0}, {SOL, 3, 1, 0}, {SOL, 3, 1, 0}, 
+{LA, 3, 1, 0}, {LA, 3, 1, 0}, {SOL, 3, 2, 1}, // Octava 3
 
+{FA, 3, 1, 0}, {FA, 3, 1, 0}, {MI, 3, 1, 0}, {MI, 3, 1, 0}, 
+{RE, 3, 1, 0}, {RE, 3, 1, 0}, {DO, 3, 2, 0}, // Octava 3
 
+{DO, 4, 1, 0}, {DO, 4, 1, 0}, {SOL, 4, 1, 0}, {SOL, 4, 1, 0}, 
+{LA, 4, 1, 0}, {LA, 4, 1, 0}, {SOL, 4, 2, 1}, // Octava 4
 
-// Nombres de las notas para imprimir
+{FA, 4, 1, 0}, {FA, 4, 1, 0}, {MI, 4, 1, 0}, {MI, 4, 1, 0}, 
+{RE, 4, 1, 0}, {RE, 4, 1, 0}, {DO, 4, 2, 0}, // Octava 4
+
+{DO, 5, 1, 0}, {DO, 5, 1, 0}, {SOL, 5, 1, 0}, {SOL, 5, 1, 0}, 
+{LA, 5, 1, 0}, {LA, 5, 1, 0}, {SOL, 5, 2, 1}, // Octava 5
+
+{FA, 5, 1, 0}, {FA, 5, 1, 0}, {MI, 5, 1, 0}, {MI, 5, 1, 0}, 
+{RE, 5, 1, 0}, {RE, 5, 1, 0}, {DO, 5, 2, 0}, // Octava 5
+
+{DO, 6, 1, 0}, {DO, 6, 1, 0}, {SOL, 6, 1, 0}, {SOL, 6, 1, 0}, 
+{LA, 6, 1, 0}, {LA, 6, 1, 0}, {SOL, 6, 2, 1}, // Octava 6
+
+{FA, 6, 1, 0}, {FA, 6, 1, 0}, {MI, 6, 1, 0}, {MI, 6, 1, 0}, 
+{RE, 6, 1, 0}, {RE, 6, 1, 0}, {DO, 6, 2, 0}, // Octava 6
+
+}; //[Nota,octava,DuracionNota,DuracionSilencio]
+
 
 //================================================== Funciones  =====================================================
+void AplicacionFrecuenciaDecimales(int frecuenciasolicitada)
+{
+	int divisor;
+	int fclk = 80000000;
+	int Res_Contador=12;
+    int denominador;
+	
+	denominador=(frecuenciasolicitada/100 *(1 << Res_Contador));
+	divisor = (fclk / denominador); 
+ 	
+ 	//int divisorentero = divisor/100;  // Parte entera
+    //int divisordecimal = divisor % 100; 
+	
+	//Asignamos el registro 
+	ValorDesplazadoDivisor = divisor << 13;
+    BitMask = REG_READ(LEDC_HSTIMER0_CONF_REG); //Configuramos el registro del timer 0 del LEd (pag 408)
+ 	BitMask &= (~0x007FE000);  // 0x00000FC0 es la máscara que limpia los bits de 5 a 12  0x00001FE0
+    BitMask |= ValorDesplazadoDivisor;
+    REG_WRITE(LEDC_HSTIMER0_CONF_REG, BitMask);
+
+}
 void TodosLosArmonicos()
 {
 	if (nota<=CantidadDeNotas)
@@ -108,8 +129,14 @@ void CambiaLaNota()
 		nota = Melodia[NumeroNota][0];
         octava = Melodia[NumeroNota][1];
         duracion = Duraciones[Melodia[NumeroNota][2]];
-		frecuencia = NotasyOctavas[nota][octava];
-		ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0, frecuencia);
+        silencio = Duraciones[Melodia[NumeroNota][3]];
+		frecuencia = (NotasyOctavas[nota][octava]);
+		//Frecuencia decimal
+		AplicacionFrecuenciaDecimales(frecuencia);
+		//Frecuecnia set
+		//int frecuenciaentera = frecuencia / 100;  // Parte entera
+		//ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0, frecuenciaentera);
+		//
 		ledc_timer_rst(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0);
 		ledc_timer_resume(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0);
 			if (NumeroNota==CantidadNotasMelodia)
@@ -118,7 +145,7 @@ void CambiaLaNota()
 				TiempoDeSilencio=2000000;
 			}
 			else{
-				TiempoDeSilencio=350000;
+				TiempoDeSilencio=silencio;
 				NumeroNota++;
 			}
 		}  
@@ -137,9 +164,9 @@ static bool IRAM_ATTR CambioNota(gptimer_handle_t timer, const gptimer_alarm_eve
 	if(NotaSonando==0)
 	{
 		CambiaLaNota();
-		 // Configuramos el ciclo de trabajo a un valor adecuado para la frecuencia deseada
-         ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 32); // 50% de ciclo de trabajo
-         ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);  // Actualizamos el ciclo de trabajo
+		// Configuramos el ciclo de trabajo a un valor adecuado para la frecuencia deseada
+        ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 32); // 50% de ciclo de trabajo
+        ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);  // Actualizamos el ciclo de trabajo
 		// Indicamos que esta sonando
 		NotaSonando=1;
 		//Duracion de sonido
@@ -219,22 +246,22 @@ void app_main(void)
 		// Configuracion del ledc timer
 	    ledc_timer_config_t PWM_timer = 
 	    {
-	        .duty_resolution = LEDC_TIMER_6_BIT, 		// resolution of PWM duty (1024)
-	        .freq_hz = 32,                      		// frequency of PWM signal
+	        .duty_resolution = LEDC_TIMER_12_BIT, 		// resolution of PWM duty (1024)
+	        .freq_hz = 100,                      		// frequency of PWM signal
 	        .speed_mode = LEDC_HIGH_SPEED_MODE,      	// timer mode
 	        .timer_num = LEDC_TIMER_0,            		// timer index
-	        .clk_cfg = LEDC_AUTO_CLK,             		// Auto select the source clock
+	        .clk_cfg = LEDC_APB_CLK,             		// Auto select the source clock
 	    };
 	    // Configuracion del canal del timer (cada ledc timer tiene dos canales)
 	    ledc_channel_config_t PWM_channel = 
 	    {
 	                .channel    = LEDC_CHANNEL_0,
-	                .duty       = 32,
+	                .duty       = 2046,
 	                .gpio_num   = 25,
 	                .speed_mode = LEDC_HIGH_SPEED_MODE,
 	                .hpoint     = 0,
 	                .timer_sel  = LEDC_TIMER_0
-	            };
+	      };
 	    // ======================================== Configuraciones de inicio ==================================
 	    // Se aplican las configuraciones
 	    ledc_timer_config(&PWM_timer);
@@ -255,7 +282,7 @@ void app_main(void)
 				{	
 					
 					frecuencia = NotasyOctavas[nota][octava];			
-					printf("Nota: %s, Octava: %d, Frecuencia: %lu Hz\n", NombresNotas[nota], octava +1 , (unsigned long)frecuencia);
+					printf("Nota: %s, Octava: %d, Frecuencia: %lu Hz\n", NombresNotas[nota], octava +1 , (unsigned long)frecuencia/100);
 					NotaActual=nota;
 				}
 			}
